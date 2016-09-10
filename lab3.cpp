c5666e3678acdcc69b05faf68e965b0104234274
MARY TIFFANY TOGONON <MCTOGONON@fit.edu.ph>
2016-09-10 11:14:19 +0800

    Initial lab3

---
+++ lab3.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int mystrcmp(char* str1, char* str2){
+  //return strlen(str);
+  switch (strcmp(str1, str2));
+}
+
+int main(){
+  char str1[100]="Hello";
+  char str2[100]="hello";
+  //strcpy(str1,"Hello there");
+  //cout << yo(str1);
+  
+  cout<<mystrcmp(str1, str2);
+}
+
