31d6932c37c20735b076aeb51a6d7ce280c769ef
mctogonon <togs.teppi@gmail.com>
2016-09-26 11:42:12 +0800

    Update (problems 1, 2, & 3

--- lab3.cpp
+++ lab3.cpp
@@ -19,19 +19,20 @@ int mystrcmp(char* str1, char* str2){
   };
 }
 
-void mystrcpy(char* str1, char* str2){
-	strcpy(str1,str2);
+void mystrcat(char* str1, char* str2){
+	strcat(str1, str2);
 }
 
-void mystrcat(char* str1, char* str2){
-	strcat(str1,str2);
+void mystrcpy(char* str1, char* str2){
+	strcpy(str1, str2);
 }
+
 
 int main(){
   char str1[100];
   char str2[100];
-  //strcpy(str1,"Hello there");
-  //cout << yo(str1);
+
+  cout<<endl<<endl;
   
   cout << "Enter a first word: ";
   cin >> str1;
@@ -39,12 +40,29 @@ int main(){
   cout << "Enter a second word: ";
   cin >> str2;
   
+  cout <<endl;
+  
+	cout <<"************************"<<endl;
+	cout <<"String Compare"<<endl;
+	cout <<"************************"<<endl;
+  
   mystrcmp(str1, str2);
-  cout<< "\n";
+  
+  cout<< endl <<endl << endl;
+  
+	cout <<"************************"<<endl;
+	cout <<"String Concatination"<<endl;
+	cout <<"************************"<<endl;
   
   mystrcat(str1, str2);
-  cout<<str1<<endl;
+  cout<<str1<<endl<<endl <<endl <<endl;
+  
+	cout <<"************************"<<endl;
+	cout <<"String Copy"<<endl;
+	cout <<"************************"<<endl;
   
   mystrcpy(str1, str2);
   cout << str1 << endl;
+  
+ 
 }
